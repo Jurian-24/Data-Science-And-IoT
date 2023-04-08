edaaed5e9d366a3907a87ddd069995ea687e66e8
Jurian-24 <jurian.andriessen@gmail.com>
2023-04-08 15:02:18 +0200

    chore(fix): Data didn't write to the given field

--- python/api.py
+++ python/api.py
@@ -3,7 +3,7 @@ import requests
 class API:
     @staticmethod
     def write(value, field = 1):
-        requests.post(f"https://api.thingspeak.com/update?api_key=N7QUSQSDA8IUJM89&field2={value}")
+        requests.post(f"https://api.thingspeak.com/update?api_key=N7QUSQSDA8IUJM89&field{field}={value}")
 
     def get(field = 2):
         response = requests.get("https://api.thingspeak.com/channels/2094803/feeds.json?api_key=X6OZE8XX2HH3L7VF&results=8000")
